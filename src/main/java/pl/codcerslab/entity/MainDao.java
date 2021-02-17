740baa00af1ef9e929b5d6bf277c12f982d65fd9
Jarosław Frankiewicz <63227341+JaroslawFrankiewicz@users.noreply.github.com>
2021-02-17 20:00:57 +0100

    Update MainDao.java

--- src/main/java/pl/codcerslab/entity/MainDao.java
+++ src/main/java/pl/codcerslab/entity/MainDao.java
@@ -8,20 +8,20 @@ public class MainDao {
         Scanner sc = new Scanner(System.in);
         // dodawanie użytkownika do bazy
         User user = new User();
-//        String name;
-//        System.out.println("Podaj nazwę użytkownika");
-//        name = sc.next();
-//        user.setUserName(name);
-//        String email;
-//        System.out.println("Podaj adres email");
-//        email = sc.next();
-//        user.setEmail(email);
-//        String pass;
-//        System.out.println("Podaj hasło");
-//        pass = sc.next();
-//        user.setPassword(pass);
+       String name;
+       System.out.println("Podaj nazwę użytkownika");
+       name = sc.next();
+       user.setUserName(name);
+       String email;
+       System.out.println("Podaj adres email");
+       email = sc.next();
+       user.setEmail(email);
+       String pass;
+       System.out.println("Podaj hasło");
+       pass = sc.next();
+       user.setPassword(pass);
         UserDao userDao = new UserDao();
-//        userDao.create(user);
+       userDao.create(user);
 
         // wczytywanie danych użytkownika z bazy
 //        UserDao userDao = new UserDao();
@@ -44,18 +44,18 @@ public class MainDao {
 
         // zmiana danych użytkownika
 //        UserDao userDao = new UserDao();
-//        System.out.println("Podaj ID użytkownika, któremu chcesz zmienić dane:");
-//        int idRead = sc.nextInt();
-//        User userToChange = userDao.read(idRead);
-//        System.out.println("Podaj nową nazwę użytkowanika:");
-//        String newName = sc.next();
-//        userToChange.setUserName(newName);
-//        System.out.println("Podaj nowy adres email:");
-//        String newEmail = sc.next();
-//        userToChange.setEmail(newEmail);
-//        System.out.println("Podaj nowe hasło:");
-//        String newPass = sc.next();
-//        userToChange.setPassword(newPass);
-//        userDao.update(userToChange);
+       System.out.println("Podaj ID użytkownika, któremu chcesz zmienić dane:");
+       int idRead = sc.nextInt();
+       User userToChange = userDao.read(idRead);
+       System.out.println("Podaj nową nazwę użytkowanika:");
+       String newName = sc.next();
+       userToChange.setUserName(newName);
+       System.out.println("Podaj nowy adres email:");
+       String newEmail = sc.next();
+       userToChange.setEmail(newEmail);
+       System.out.println("Podaj nowe hasło:");
+       String newPass = sc.next();
+       userToChange.setPassword(newPass);
+       userDao.update(userToChange);
     }
 }
